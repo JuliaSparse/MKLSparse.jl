1f9961313acb37e36e98a3a7df73274d66507108
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2015-08-18 18:46:54 +0200

    unindented code a bit

--- src/generator.jl
+++ src/generator.jl
@@ -20,55 +20,62 @@ for (mv, sv, mm, sm, T) in ((:mkl_scscmv_, :mkl_scscsv_, :mkl_scscmm_, :mkl_scsc
             return y
         end
 
-        function cscsv!(transa::Char, α::$T, matdescra::ASCIIString, A::SparseMatrixCSC{$T, BlasInt}, x::StridedVector{$T}, y::StridedVector{$T})
-            A.m == A.n || throw(DimensionMismatch("Matrix must be square"))
-            length(x) == A.n || throw(DimensionMismatch("Matrix with $(A.n) columns multiplied with vector of length $(length(x))"))
-            length(y) >= A.m || throw(DimensionMismatch("Vector of length $(A.m) assigned to vector of length $(length(y))"))
-            ccall(($(string(sv)), :libmkl_rt), Void,
-                (Ptr{Uint8}, Ptr{BlasInt}, Ptr{$T}, Ptr{Uint8},
-                 Ptr{$T}, Ptr{BlasInt}, Ptr{BlasInt}, Ptr{BlasInt},
-                 Ptr{$T}, Ptr{$T}),
-                &transa, &A.m, &α, matdescra,
-                A.nzval, A.rowval, pointer(A.colptr, 1), pointer(A.colptr, 2),
-                x, y)
-            return y
-        end
+function cscsv!(transa::Char, α::$T, matdescra::ASCIIString,
+                A::SparseMatrixCSC{$T, BlasInt}, x::StridedVector{$T},
+                y::StridedVector{$T})
+    A.m == A.n || throw(DimensionMismatch("Matrix must be square"))
+    length(x) == A.n || throw(DimensionMismatch("Matrix with $(A.n) columns multiplied with vector of length $(length(x))"))
+    length(y) >= A.m || throw(DimensionMismatch("Vector of length $(A.m) assigned to vector of length $(length(y))"))
+    ccall(($(string(sv)), :libmkl_rt), Void,
+        (Ptr{Uint8}, Ptr{BlasInt}, Ptr{$T}, Ptr{Uint8},
+         Ptr{$T}, Ptr{BlasInt}, Ptr{BlasInt}, Ptr{BlasInt},
+         Ptr{$T}, Ptr{$T}),
+        &transa, &A.m, &α, matdescra,
+        A.nzval, A.rowval, pointer(A.colptr, 1), pointer(A.colptr, 2),
+        x, y)
+    return y
+end
 
-        function cscmm!(transa::Char, α::$T, matdescra::ASCIIString, A::SparseMatrixCSC{$T, BlasInt}, B::StridedMatrix{$T}, β::$T, C::StridedMatrix{$T})
-            mB, nB = size(B)
-            mC, nC = size(C)
-            A.n == mB || throw(DimensionMismatch("Matrix with $(A.n) columns multiplied with matrix with $(mB) rows"))
-            A.m == mC || throw(DimensionMismatch("Matrix with $(A.m) rows added to matrix with $(mC) rows"))
-            nB == nC || throw(DimensionMismatch("Matrix with $(nB) columns added to matrix with $(nC) columns"))
-            ccall(($(string(mm)), :libmkl_rt), Void,
-                (Ptr{Uint8}, Ptr{BlasInt}, Ptr{BlasInt}, Ptr{BlasInt},
-                 Ptr{$T}, Ptr{Uint8}, Ptr{$T}, Ptr{BlasInt},
-                 Ptr{BlasInt}, Ptr{BlasInt}, Ptr{$T}, Ptr{BlasInt},
-                 Ptr{$T}, Ptr{$T}, Ptr{BlasInt}),
-                &transa, &A.m, &nC, &A.n,
-                &α, matdescra, A.nzval, A.rowval,
-                pointer(A.colptr, 1), pointer(A.colptr, 2), B, &mB,
-                &β, C, &mC)
-            return C
-        end
+function cscmm!(transa::Char, α::$T, matdescra::ASCIIString,
+                A::SparseMatrixCSC{$T, BlasInt}, B::StridedMatrix{$T},
+                β::$T, C::StridedMatrix{$T})
+    mB, nB = size(B)
+    mC, nC = size(C)
+    A.n == mB || throw(DimensionMismatch("Matrix with $(A.n) columns multiplied with matrix with $(mB) rows"))
+    A.m == mC || throw(DimensionMismatch("Matrix with $(A.m) rows added to matrix with $(mC) rows"))
+    nB == nC || throw(DimensionMismatch("Matrix with $(nB) columns added to matrix with $(nC) columns"))
+    ccall(($(string(mm)), :libmkl_rt), Void,
+        (Ptr{Uint8}, Ptr{BlasInt}, Ptr{BlasInt}, Ptr{BlasInt},
+         Ptr{$T}, Ptr{Uint8}, Ptr{$T}, Ptr{BlasInt},
+         Ptr{BlasInt}, Ptr{BlasInt}, Ptr{$T}, Ptr{BlasInt},
+         Ptr{$T}, Ptr{$T}, Ptr{BlasInt}),
+        &transa, &A.m, &nC, &A.n,
+        &α, matdescra, A.nzval, A.rowval,
+        pointer(A.colptr, 1), pointer(A.colptr, 2), B, &mB,
+        &β, C, &mC)
+    return C
+end
 
-        function cscsm!(transa::Char, α::$T, matdescra::ASCIIString, A::SparseMatrixCSC{$T, BlasInt}, B::StridedMatrix{$T}, C::StridedMatrix{$T})
-            mB, nB = size(B)
-            mC, nC = size(C)
-            A.m == A.n || throw(DimensionMismatch("Matrix must be square"))
-            A.n == mB || throw(DimensionMismatch("Matrix with $(A.n) columns multiplied with matrix with $(mB) rows"))
-            A.m <= mC || throw(DimensionMismatch("Matrix with $(A.m) rows assigned to matrix with $(mC) rows"))
-            nB <= nC || throw(DimensionMismatch("Matrix with $(nB) columns assigned to matrix with $(nC) columns"))
-            ccall(($(string(sm)), :libmkl_rt), Void,
-                (Ptr{Uint8}, Ptr{BlasInt}, Ptr{BlasInt}, Ptr{$T},
-                 Ptr{Uint8}, Ptr{$T}, Ptr{BlasInt}, Ptr{BlasInt},
-                 Ptr{BlasInt}, Ptr{$T}, Ptr{BlasInt}, Ptr{$T},
-                 Ptr{BlasInt}),
-                &transa, &A.n, &nC, &α,
-                matdescra, A.nzval, A.rowval, pointer(A.colptr, 1),
-                pointer(A.colptr, 2), B, &mB, C,
-                &mC)
-            return C
-        end
-    end
+function cscsm!(transa::Char, α::$T, matdescra::ASCIIString,
+                A::SparseMatrixCSC{$T, BlasInt}, B::StridedMatrix{$T},
+                C::StridedMatrix{$T})
+    mB, nB = size(B)
+    mC, nC = size(C)
+    A.m == A.n || throw(DimensionMismatch("Matrix must be square"))
+    A.n == mB || throw(DimensionMismatch("Matrix with $(A.n) columns multiplied with matrix with $(mB) rows"))
+    A.m <= mC || throw(DimensionMismatch("Matrix with $(A.m) rows assigned to matrix with $(mC) rows"))
+    nB <= nC || throw(DimensionMismatch("Matrix with $(nB) columns assigned to matrix with $(nC) columns"))
+    ccall(($(string(sm)), :libmkl_rt), Void,
+        (Ptr{Uint8}, Ptr{BlasInt}, Ptr{BlasInt}, Ptr{$T},
+         Ptr{Uint8}, Ptr{$T}, Ptr{BlasInt}, Ptr{BlasInt},
+         Ptr{BlasInt}, Ptr{$T}, Ptr{BlasInt}, Ptr{$T},
+         Ptr{BlasInt}),
+        &transa, &A.n, &nC, &α,
+        matdescra, A.nzval, A.rowval, pointer(A.colptr, 1),
+        pointer(A.colptr, 2), B, &mB, C,
+        &mC)
+    return C
+end
+end
+>>>>>>> 7cf19f1... unindented code a bit
 end
