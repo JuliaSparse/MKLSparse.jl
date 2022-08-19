ad831347e7a3e9ae4984efca3cb0ca09ffef5019
Alexey Stukalov <astukalov@seer.bio>
2022-08-18 17:56:47 -0700

    tests: refactor, test all BLAS eltypes

--- test/test_BLAS.jl
+++ test/test_BLAS.jl
@@ -28,76 +28,110 @@ end
     @test MKLSparse.matdescra(sA) == "GUUF"
 end
 
-const atol = 100*eps() # absolute tolerance for SparseBLAS results
+@testset "SparseBLAS for $T matrices and vectors" for T in (Float32, Float64, ComplexF32, ComplexF64) 
 
-@testset "matrix-vector multiplication (non-square)" begin
-    for i = 1:5
-        a = sprand(10, 5, 0.5)
-        b = rand(5)
-        @test @blas(a*b) ≈ Array(a)*b atol=atol
-        b = rand(5, 5)
-        @test @blas(a*b) ≈ Array(a)*b atol=atol
-        b = rand(10)
-        @test @blas(a'*b) ≈ Array(a)'*b atol=atol
-        @test @blas(transpose(a)*b) ≈ Array(a)'*b atol=atol
-        b = rand(10,10)
-        @test @blas(a'*b) ≈ Array(a)'*b atol=atol
-        @test @blas(transpose(a)*b) ≈ Array(a)'*b atol=atol
+local atol::real(T) = 100*eps(real(one(T))) # absolute tolerance for SparseBLAS results
+
+@testset "SparseMatrixCSC{$T} * Vector{$T}" begin
+    for _ in 1:10
+        spA = sprand(T, 10, 5, 0.5)
+        a = Array(spA)
+        b = rand(T, 5)
+        c = rand(T, 10)
+
+        @test @blas(spA*b) ≈ a*b atol=atol
+        @test @blas(spA'*c) ≈ a'*c atol=atol
+        @test @blas(transpose(spA)*c) ≈ transpose(a)*c atol=atol
+
+        @test_throws DimensionMismatch spA*c
+        @test_throws DimensionMismatch spA'*b
+        @test_throws DimensionMismatch transpose(spA)*b
     end
 end
 
-#?
-@testset "complex matrix-vector multiplication" begin
-    for i = 1:5
-        a = I + im * 0.1*sprandn(5, 5, 0.2)
-        b = randn(5,3) + im*randn(5,3)
-        c = randn(5) + im*randn(5)
-        d = randn(5) + im*randn(5)
-        α = rand(ComplexF64)
-        β = rand(ComplexF64)
-        @test @blas(a*b) ≈ Array(a)*b atol=atol
-        @test @blas(a'*b) ≈ Array(a)'*b atol=atol
-        @test @blas(transpose(a)*b) ≈ transpose(Array(a))*b atol=atol
-        @test @blas(mul!(similar(b), a, b)) ≈ Array(a)*b atol=atol
-        @test @blas(mul!(similar(c), a, c)) ≈ Array(a)*c atol=atol
-        @test @blas(mul!(similar(b), transpose(a), b)) ≈ transpose(Array(a))*b atol=atol
-        @test @blas(mul!(similar(c), transpose(a), c)) ≈ transpose(Array(a))*c atol=atol
-        @test @blas(mul!(copy(b), a, b, α, β)) ≈ (α*(Array(a)*b) + β*b) atol=atol
-        @test @blas(mul!(copy(b), transpose(a), b, α, β)) ≈ (α*(transpose(Array(a))*b) + β*b) atol=atol
-        @test @blas(mul!(copy(c), transpose(a), c, α, β)) ≈ (α*(transpose(Array(a))*c) + β*c) atol=atol
-        α = β = 1 # test conversion to float
-        @test @blas(mul!(copy(b), a, b, α, β)) ≈ (α*(Array(a)*b) + β*b) atol=atol
-        @test @blas(mul!(copy(b), transpose(a), b, α, β)) ≈ (α*(transpose(Array(a))*b) + β*b) atol=atol
-        @test @blas(mul!(copy(c), transpose(a), c, α, β)) ≈ (α*(transpose(Array(a))*c) + β*c) atol=atol
+@testset "Vector{$T} * SparseMatrixCSC{$T}" begin
+    for _ in 1:10
+        spA = sprand(T, 10, 5, 0.5)
+        a = Array(spA)
+        b = rand(T, 10)
+        c = rand(T, 5)
 
-        c = randn(6) + im*randn(6)
-        @test_throws DimensionMismatch transpose(a)*c
-        @test_throws DimensionMismatch a.*c
-        @test_throws DimensionMismatch a.*c
+        @test @blas(b'*spA) ≈ b'*a atol=atol
+        @test @blas(c'*spA') ≈ c'*a' atol=atol
+
+        @test_throws DimensionMismatch c*spA
+        @test_throws DimensionMismatch b*spA'
+        @test_throws DimensionMismatch b*transpose(spA)
+
+        @test_throws DimensionMismatch c'*spA
+        @test_throws DimensionMismatch b'*spA'
+
+        if !(T <: Complex) # adjoint*transposed isn't routed to BLAS call
+            @test @blas(c'*transpose(spA)) ≈ c'*transpose(a) atol=atol
+            @test_throws DimensionMismatch b'*transpose(spA)
+        end
     end
 end
 
-@testset "triangular" begin
-    n = 100
-    A = sprandn(n, n, 0.5) + sqrt(n)*I
-    b = rand(n)
-    symA = A + transpose(A)
-    trilA = tril(A)
-    triuA = triu(A)
-    trilUA = tril(A, -1) + I
-    triuUA = triu(A, 1)  + I
+@testset "SparseMatrixCSC{$T} * Matrix{$T}" begin
+    for _ in 1:10
+        spA = sprand(T, 10, 5, 0.5)
+        a = Array(spA)
+        b = rand(T, 5, 8)
+        c = rand(T, 10, 12)
+        ab = rand(T, 10, 8)
+        tac = rand(T, 5, 12)
+        α = rand(T)
+        β = rand(T)
 
-    @test @blas(LowerTriangular(trilA) \ b) ≈ Array(LowerTriangular(trilA)) \ b
-    @test @blas(LowerTriangular(trilA) * b) ≈ Array(LowerTriangular(trilA)) * b
+        @test @blas(spA*b) ≈ a*b atol=atol
+        @test @blas(spA'*c) ≈ a'*c atol=atol
+        @test @blas(transpose(spA)*c) ≈ transpose(a)*c atol=atol
 
-    @test @blas(UpperTriangular(triuA) \ b) ≈ Array(UpperTriangular(triuA)) \ b
-    @test @blas(UpperTriangular(triuA) * b) ≈ Array(UpperTriangular(triuA)) * b
+        @test_throws DimensionMismatch spA*c
+        @test_throws DimensionMismatch spA'*b
+        @test_throws DimensionMismatch transpose(spA)*b
 
-    @test @blas(UnitLowerTriangular(trilUA) \ b) ≈ Array(UnitLowerTriangular(trilUA)) \ b
-    @test @blas(UnitLowerTriangular(trilUA) * b) ≈ Array(UnitLowerTriangular(trilUA)) * b
+        @test @blas(mul!(similar(ab), spA, b)) ≈ a*b atol=atol
+        @test @blas(mul!(similar(tac), spA', c)) ≈ a'*c atol=atol
+        @test @blas(mul!(similar(tac), transpose(spA), c)) ≈ transpose(a)*c atol=atol
 
-    @test @blas(UnitUpperTriangular(triuUA) \ b) ≈ Array(UnitUpperTriangular(triuUA)) \ b
-    @test @blas(UnitUpperTriangular(triuUA) * b) ≈ Array(UnitUpperTriangular(triuUA)) * b
+        @test @blas(mul!(copy(ab), spA, b, α, β)) ≈ α*a*b + β*ab atol=atol
+        @test @blas(mul!(copy(tac), spA', c, α, β)) ≈ α*a'*c + β*tac atol=atol
+        @test @blas(mul!(copy(tac), transpose(spA), c, α, β)) ≈ α*transpose(a)*c + β*tac atol=atol
 
-    @test @blas(Symmetric(symA) * b) ≈ Array(Symmetric(symA)) * b
+        @test @blas(mul!(copy(ab), spA, b, 1, 1)) ≈ a*b + ab atol=atol
+        @test @blas(mul!(copy(tac), transpose(spA), c, 1, 1)) ≈ transpose(a)*c + tac atol=atol
+        @test @blas(mul!(copy(tac), spA', c, 1, 1)) ≈ a'*c + tac atol=atol
+    end
+end
+
+@testset "SparseTriangular{$T} {* /} Vector{$T}" begin
+    for _ in 1:10
+        n = rand(50:150)
+        spA = sprand(T, n, n, 0.5) + convert(real(T), sqrt(n))*I
+        A = Array(spA)
+        b = rand(T, n)
+        symA = spA + transpose(spA)
+        trilA = tril(spA)
+        triuA = triu(spA)
+        trilUA = tril(spA, -1) + I
+        triuUA = triu(spA, 1)  + I
+
+        @test @blas(LowerTriangular(trilA) \ b) ≈ Array(LowerTriangular(trilA)) \ b
+        @test @blas(LowerTriangular(trilA) * b) ≈ Array(LowerTriangular(trilA)) * b
+
+        @test @blas(UpperTriangular(triuA) \ b) ≈ Array(UpperTriangular(triuA)) \ b
+        @test @blas(UpperTriangular(triuA) * b) ≈ Array(UpperTriangular(triuA)) * b
+
+        @test @blas(UnitLowerTriangular(trilUA) \ b) ≈ Array(UnitLowerTriangular(trilUA)) \ b
+        @test @blas(UnitLowerTriangular(trilUA) * b) ≈ Array(UnitLowerTriangular(trilUA)) * b
+
+        @test @blas(UnitUpperTriangular(triuUA) \ b) ≈ Array(UnitUpperTriangular(triuUA)) \ b
+        @test @blas(UnitUpperTriangular(triuUA) * b) ≈ Array(UnitUpperTriangular(triuUA)) * b
+
+        @test @blas(Symmetric(symA) * b) ≈ Array(Symmetric(symA)) * b
+    end
+end
+
 end
