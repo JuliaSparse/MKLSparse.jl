cfdbe394a73d0f369d0f4d15e865dc174971dad7
Alexey Stukalov <astukalov@seer.bio>
2022-08-20 10:12:54 -0700

    cleanup&generalize matrices size checks

--- src/generator.jl
+++ src/generator.jl
@@ -1,28 +1,23 @@
-# The increments to the `__counter` variable is for testing purposes
-
 function _check_transa(t::Char)
     if !(t in ('C', 'N', 'T'))
-        error("transa: is '$t', must be 'N', 'T', or 'C'")
+        throw(ArgumentError("transa: is '$t', must be 'N', 'T', or 'C'"))
     end
 end
 
-mkl_size(t::Char, M::AbstractVecOrMat) = t == 'N' ? size(M) : reverse(size(M))
-mkl_size(t::Char, M::AbstractVecOrMat, ix::Integer) = t == 'N' ? size(M, ix) : size(M, ndims(M) - ix + 1)
+# Checks sizes for the multiplication C <- tA[A] * tB[B]
+function _check_mat_mult_matvec(C, A, tA, B, tB)
+    _size(t::Char, M::AbstractMatrix) = t == 'N' ? size(M) : reverse(size(M))
+    _size(t::Char, V::AbstractVector) = t == 'N' ? (size(V, 1), 1) : (1, size(V, 1))
+    _str(M::AbstractMatrix) = string("[", size(M, 1), ", ", size(M, 2), "]")
+    _str(V::AbstractVector) = string("[", size(V, 1), "]")
+    _t(t) = t == 'T' ? "ᵀ" : t == 'C' ? "ᴴ" : t == 'N' ? "" : "ERROR"
 
-# Checks sizes for the multiplication C <- A * B
-function _check_mat_mult_matvec(C, A, B, tA)
-    _size(v::AbstractMatrix) = size(v)
-    _size(v::AbstractVector) = (size(v,1), 1)
-    _str(v::AbstractMatrix) = string("[", size(v,1), ", ", size(v,2), "]")
-    _str(v::AbstractVector) = string("[", size(v,1), "]")
-    mA, nA = mkl_size(tA, A)
-    mB, nB = _size(B)
-    mC, nC = _size(C)
+    mA, nA = _size(tA, A)
+    mB, nB = _size(tB, B)
+    mC, nC = _size('N', C)
     if nA != mB || mC != mA || nC != nB
-        t = ""
-        if tA == 'T'; t = ".\'"; end
-        if tA == 'C'; t = "\'"; end
-        str = string("arrays had inconsistent dimensions for C <- A", t, " * B: ", _str(C), " <- ", _str(A), t, " * ", _str(B))
+        str = string("arrays had inconsistent dimensions for C = A", _t(tA), " * B", _t(tB), ": ",
+                     _str(C), " = ", _str(A), _t(tA), " * ", _str(B), _t(tB))
         throw(DimensionMismatch(str))
     end
 end
@@ -31,7 +26,7 @@ function cscmv!(transa::Char, α::T, matdescra::String,
                 A::SparseMatrixCSC{T, BlasInt}, x::StridedVector{T},
                 β::T, y::StridedVector{T}) where {T <: BlasFloat}
     _check_transa(transa)
-    _check_mat_mult_matvec(y, A, x, transa)
+    _check_mat_mult_matvec(y, A, transa, x, 'N')
     __counter[] += 1
 
     T == Float32    && (mkl_scscmv(transa, A.m, A.n, α, matdescra, A.nzval, A.rowval, A.colptr, pointer(A.colptr, 2), x, β, y))
@@ -45,10 +40,11 @@ function cscmm!(transa::Char, α::T, matdescra::String,
                 A::SparseMatrixCSC{T, BlasInt}, B::StridedMatrix{T},
                 β::T, C::StridedMatrix{T}) where {T <: BlasFloat}
     _check_transa(transa)
-    _check_mat_mult_matvec(C, A, B, transa)
+    _check_mat_mult_matvec(C, A, transa, B, 'N')
     mB, nB = size(B)
     mC, nC = size(C)
     __counter[] += 1
+
     T == Float32    && (mkl_scscmm(transa, A.m, nC, A.n, α, matdescra, A.nzval, A.rowval, A.colptr, pointer(A.colptr, 2), B, mB, β, C, mC))
     T == Float64    && (mkl_dcscmm(transa, A.m, nC, A.n, α, matdescra, A.nzval, A.rowval, A.colptr, pointer(A.colptr, 2), B, mB, β, C, mC))
     T == ComplexF32 && (mkl_ccscmm(transa, A.m, nC, A.n, α, matdescra, A.nzval, A.rowval, A.colptr, pointer(A.colptr, 2), B, mB, β, C, mC))
@@ -61,8 +57,9 @@ function cscsv!(transa::Char, α::T, matdescra::String,
                 y::StridedVector{T}) where {T <: BlasFloat}
     n = checksquare(A)
     _check_transa(transa)
-    _check_mat_mult_matvec(y, A, x, transa)
+    _check_mat_mult_matvec(y, A, transa, x, 'N')
     __counter[] += 1
+
     T == Float32    && (mkl_scscsv(transa, A.m, α, matdescra, A.nzval, A.rowval, A.colptr, pointer(A.colptr, 2), x, y))
     T == Float64    && (mkl_dcscsv(transa, A.m, α, matdescra, A.nzval, A.rowval, A.colptr, pointer(A.colptr, 2), x, y))
     T == ComplexF32 && (mkl_ccscsv(transa, A.m, α, matdescra, A.nzval, A.rowval, A.colptr, pointer(A.colptr, 2), x, y))
@@ -77,8 +74,9 @@ function cscsm!(transa::Char, α::T, matdescra::String,
     mC, nC = size(C)
     n = checksquare(A)
     _check_transa(transa)
-    _check_mat_mult_matvec(C, A, B, transa)
+    _check_mat_mult_matvec(C, A, transa, B, 'N')
     __counter[] += 1
+
     T == Float32    && (mkl_scscsm(transa, A.n, nC, α, matdescra, A.nzval, A.rowval, A.colptr, pointer(A.colptr, 2), B, mB, C, mC))
     T == Float64    && (mkl_dcscsm(transa, A.n, nC, α, matdescra, A.nzval, A.rowval, A.colptr, pointer(A.colptr, 2), B, mB, C, mC))
     T == ComplexF32 && (mkl_ccscsm(transa, A.n, nC, α, matdescra, A.nzval, A.rowval, A.colptr, pointer(A.colptr, 2), B, mB, C, mC))
