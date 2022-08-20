0362c81c3229a2abf42dd52c3a9b539b22b6aa59
Alexey Stukalov <astukalov@seer.bio>
2022-08-20 00:46:05 -0700

    cleanup mul!() piracy
    - separate method defs for vector and matrix
      instead of a branch within a single method
    - don't redefine Base methods that already call
      MKLSparse methods
    -  describe_and_unwrap() for deduplicating
       the sparse matrix preparation logic

--- src/matmul.jl
+++ src/matmul.jl
@@ -1,14 +1,16 @@
 import Base: *, \
 import LinearAlgebra: mul!, ldiv!
 
-_get_data(A::LowerTriangular) = tril(A.data)
-_get_data(A::UpperTriangular) = triu(A.data)
-_get_data(A::UnitLowerTriangular) = tril(A.data)
-_get_data(A::UnitUpperTriangular) = triu(A.data)
-_get_data(A::Symmetric) = A.data
+_data(A::AbstractMatrix) = A
+_data(A::LowerTriangular) = tril(A.data)
+_data(A::UpperTriangular) = triu(A.data)
+_data(A::UnitLowerTriangular) = tril(A.data)
+_data(A::UnitUpperTriangular) = triu(A.data)
+_data(A::Symmetric) = A.data
 
-_unwrap_adj(x::Union{Adjoint,Transpose}) = parent(x)
-_unwrap_adj(x) = x
+# returns a tuple of matdescra and unwrapped A
+describe_and_unwrap(A::AbstractMatrix) = (matdescra(A), _data(A)) # unwrap adjoint/transpose
+describe_and_unwrap(A::Union{Adjoint,Transpose}) = describe_and_unwrap(parent(A)) # unwrap adjoint/transpose
 
 const SparseMatrices{T} = Union{SparseMatrixCSC{T,BlasInt},
                         Symmetric{T,SparseMatrixCSC{T,BlasInt}},
@@ -17,84 +19,53 @@ const SparseMatrices{T} = Union{SparseMatrixCSC{T,BlasInt},
                         UpperTriangular{T, SparseMatrixCSC{T,BlasInt}},
                         UnitUpperTriangular{T, SparseMatrixCSC{T,BlasInt}}}
 
-for T in [Complex{Float32}, Complex{Float64}, Float32, Float64]
-for mat in (:StridedVector, :StridedMatrix)
-for ttype in (nothing, :Adjoint, :Transpose)
+for T in (Complex{Float32}, Complex{Float64}, Float32, Float64),
+    ttype in (nothing, :Adjoint, :Transpose)
+
     tchar = mkl_operation_code(ttype)
     AT = isnothing(ttype) ? :(SparseMatrixCSC{$T,BlasInt}) : :($ttype{$T,SparseMatrixCSC{$T,BlasInt}})
     @eval begin
-        function mul!(C::$mat{$T}, adjA::$AT, B::$mat{$T}, α::Number, β::Number)
-            A = _unwrap_adj(adjA)
-            if isa(B, AbstractVector)
-                return cscmv!($tchar, $T(α), matdescra(A), A, B, $T(β), C)
-            else
-                return cscmm!($tchar, $T(α), matdescra(A), A, B, $T(β), C)
-            end
-        end
+        LinearAlgebra.mul!(C::StridedVector{$T}, A::$AT, B::StridedVector{$T}, α::Number, β::Number) =
+            cscmv!($tchar, $T(α), describe_and_unwrap(A)..., B, $T(β), C)
 
-        mul!(C::$mat{$T}, adjA::$AT, B::$mat{$T}) = mul!(C, adjA, B, one($T), zero($T))
+        LinearAlgebra.mul!(C::StridedMatrix{$T}, A::$AT, B::StridedMatrix{$T}, α::Number, β::Number) =
+            cscmm!($tchar, $T(α), describe_and_unwrap(A)..., B, $T(β), C)
 
-        function (*)(adjA::$AT, B::$mat{$T})
-            A = _unwrap_adj(adjA)
-            if isa(B, AbstractVector)
-                return mul!(zeros($T, mkl_size($tchar, A, 1)),             adjA, B)
-            else
-                return mul!(zeros($T, mkl_size($tchar, A, 1), size(B, 2)), adjA, B)
-            end
-        end
+        LinearAlgebra.mul!(C::StridedMatrix{$T}, A::StridedMatrix{$T}, B::$AT, α::Number, β::Number) =
+            cscmm!($tchar, $T(α), A, describe_and_unwrap(B)..., $T(β), C)
     end
 
     for w in (:Symmetric, :LowerTriangular, :UnitLowerTriangular, :UpperTriangular, :UnitUpperTriangular)
         AT = isnothing(ttype) ?
             :($w{$T,SparseMatrixCSC{$T,BlasInt}}) :
             :($ttype{$T,$w{$T,SparseMatrixCSC{$T,BlasInt}}})
+        BT = :(Union{StridedMatrix{$T}, StridedVector{$T}})
+
         @eval begin
-            function mul!(C::$mat{$T}, adjA::$AT, B::$mat{$T}, α::Number, β::Number)
-                A = _unwrap_adj(adjA)
-                if isa(B, AbstractVector)
-                    return cscmv!($tchar, $T(α), matdescra(A), _get_data(A), B, $T(β), C)
-                else
-                    return cscmm!($tchar, $T(α), matdescra(A), _get_data(A), B, $T(β), C)
-                end
-            end
+            LinearAlgebra.mul!(C::StridedVector{$T}, A::$AT, B::StridedVector{$T}, α::Number, β::Number) =
+                cscmv!($tchar, $T(α), describe_and_unwrap(A)..., B, $T(β), C)
 
-            mul!(C::$mat{$T}, adjA::$AT, B::$mat{$T}) = mul!(C, adjA, B, one($T), zero($T))
+            LinearAlgebra.mul!(C::StridedMatrix{$T}, A::$AT, B::StridedMatrix{$T}, α::Number, β::Number) =
+                cscmm!($tchar, $T(α), describe_and_unwrap(A)..., B, $T(β), C)
 
-            function (*)(adjA::$AT, B::$mat{$T})
-                A = _unwrap_adj(adjA)
-                if isa(B, AbstractVector)
-                    return mul!(zeros($T, mkl_size($tchar, A, 1)),             adjA, B)
-                else
-                    return mul!(zeros($T, mkl_size($tchar, A, 1), size(B, 2)), adjA, B)
-                end
-            end
+            LinearAlgebra.mul!(C::BT, A::$AT, B::BT) where BT <: $BT = mul!(C, A, B, one($T), zero($T))
+
+            Base.:(*)(A::$AT, B::StridedVector{$T}) = mul!(Vector{$T}(undef, size(A, 1)), A, B)
+            Base.:(*)(A::$AT, B::StridedMatrix{$T}) = mul!(Matrix{$T}(undef, size(A, 1), size(B, 2)), A, B)
         end
 
         if w != :Symmetric
             @eval begin
-                function ldiv!(α::Number, adjA::$AT,
-                               B::$mat{$T}, C::$mat{$T})
-                    A = _unwrap_adj(adjA)
-                    if isa(B,AbstractVector)
-                        return cscsv!($tchar, $T(α), matdescra(A), _get_data(A), B, C)
-                    else
-                        return cscsm!($tchar, $T(α), matdescra(A), _get_data(A), B, C)
-                    end
-                end
+                LinearAlgebra.ldiv!(α::Number, A::$AT, B::StridedVector{$T}, C::StridedVector{$T}) =
+                    cscsv!($tchar, $T(α), describe_and_unwrap(A)..., B, C)
+                LinearAlgebra.ldiv!(α::Number, A::$AT, B::StridedMatrix{$T}, C::StridedMatrix{$T}) =
+                    cscsm!($tchar, $T(α), describe_and_unwrap(A)..., B, C)
 
-                ldiv!(C::$mat{$T}, A::$AT, B::$mat{$T}) =
-                    ldiv!(one($T), A, B, C)
+                LinearAlgebra.ldiv!(C::BT, A::$AT, B::BT) where BT <: $BT = ldiv!(one($T), A, B, C)
 
-                function (\)(A::$AT, B::$mat{$T})
-                    if isa(B,AbstractVector)
-                        return ldiv!(zeros($T, size(A, 1)),             A, B)
-                    else
-                        return ldiv!(zeros($T, size(A, 1), size(B, 2)), A, B)
-                    end
-                end
+                Base.:(\)(A::$AT, B::StridedVector{$T}) = ldiv!(Vector{$T}(undef, size(A, 1)), A, B)
+                Base.:(\)(A::$AT, B::StridedMatrix{$T}) = ldiv!(Matrix{$T}(undef, size(A, 1), size(B, 2)), A, B)
             end
         end
     end
-end
-end # mat
-end # T
+end # T, ttype
