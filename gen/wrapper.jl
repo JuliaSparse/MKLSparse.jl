beb40be26bb636dea995a4989c9f8c9e9f9bcfc2
Alexey Stukalov <alyst@users.noreply.github.com>
2024-09-16 14:09:50 -0700

    wrapper: skip MKL_UPPERCASE functions (#51)
    Co-authored-by: Alexey Stukalov <astukalol@seer.bio>

--- gen/wrapper.jl
+++ gen/wrapper.jl
@@ -13,16 +13,13 @@ function wrapper(name::String, headers::Vector{String}, optimized::Bool=false)
   include_dir = joinpath(MKL_Headers_jll.artifact_dir, "include")
 
   options = load_options(joinpath(@__DIR__, "mkl.toml"))
-  ignore_list = ["MKL_Complex8", "MKL_Complex16", "MKLVersion", "MKL_INT64",
-  "MKL_INT", "MKL_UINT", "MKL_UINT8", "MKL_INT8", "MKL_INT16", "MKL_BF16", "MKL_INT32",
-  "MKL_F16", "MKL_DOMAIN_ALL", "MKL_DOMAIN_BLAS", "MKL_DOMAIN_FFT", "MKL_DOMAIN_VML",
-  "MKL_DOMAIN_PARDISO", "MKL_DOMAIN_LAPACK", "MKL_CBWR_BRANCH", "MKL_CBWR_ALL", "MKL_UINT64",
-  "MKL_CBWR_STRICT", "MKL_CBWR_OFF", "MKL_CBWR_UNSET_ALL", "MKL_CBWR_BRANCH_OFF",
-  "MKL_CBWR_AUTO", "MKL_CBWR_COMPATIBLE", "MKL_CBWR_SSE2", "MKL_CBWR_SSSE3", "_MKL_Complex8",
-  "MKL_CBWR_SSE4_1", "MKL_CBWR_SSE4_2", "MKL_CBWR_AVX", "MKL_CBWR_AVX2", "MKL_CBWR_AVX512_MIC",
-  "MKL_CBWR_AVX512", "MKL_CBWR_AVX512_MIC_E1", "MKL_CBWR_AVX512_E1", "MKL_CBWR_SUCCESS",
-  "MKL_CBWR_ERR_INVALID_SETTINGS", "MKL_CBWR_ERR_INVALID_INPUT", "MKL_CBWR_ERR_UNSUPPORTED_BRANCH",
-  "MKL_CBWR_ERR_UNKNOWN_BRANCH", "MKL_CBWR_ERR_MODE_CHANGE_FAILURE", "MKL_CBWR_SSE3", "_MKL_Complex16"]
+  ignore_list = [
+    # exclude mkl_types.h definitions not related to MKLSparse
+    "MKLVersion",
+    "MKL_U?INT\\d*", "MKL_B?F\\d+", "_?MKL_Complex\\d+",
+    "MKL_DOMAIN_[A-Z]+", "MKL_CBWR_[A-Z0-9_]+",
+    # exclude uppercase functions
+    "MKL_[A-Z_]+(BSR|COO|CSR|CSC|DIA|SKY)[A-Z0-9_]*"]
   optimized && (options["general"]["output_ignorelist"] = ignore_list)
 
   args = get_default_args()
